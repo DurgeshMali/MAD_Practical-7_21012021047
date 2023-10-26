9d78055e3032896ac7928eb0d7a1609174a46214
Durgesh <durgeshmali5824@gmail.com>
2023-10-26 14:42:33 +0530

    pr_7

--- app/src/main/java/com/example/mad_practical_7_21012021047/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_7_21012021047/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mad_practical_7_21012021047
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +10,7 @@ import android.widget.VideoView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,5 +30,12 @@ class MainActivity : AppCompatActivity() {
                 startActivity(it)
             }
         }
+
+//        val button1: FloatingActionButton =findViewById(R.id.floatingActionButton2)
+//        button1.setOnClickListener {
+//            Intent(this@MainActivity, MainActivity::class.java).also {
+//                startActivity(it)
+//            }
+//        }
     }
 }
